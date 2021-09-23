1b8e24179ee00cf7c740758163b8047fb1f5a4b7
”sovsianykov” <swebproger@gmail.com>
2021-09-23 15:12:21 +0300

    gitignore6

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import mongoMessages from "./messageModel.js"
 import mongoose from "mongoose";
 
 const app = express();
-const PORT =  5000;
+const port = process.env.PORT || 5000;
 
 
 //middlewares
@@ -15,8 +15,8 @@ app.use(cors())
 async function startApp() {
     try {
         await mongoose.connect(mongo_Uri)
-        app.listen(PORT,() => {
-            console.log('SERVER IS RUNNING ON PORT ' + PORT)} )
+        app.listen(port,() => {
+            console.log('SERVER IS RUNNING ON PORT ' + port)} )
 
     }
     catch (e) {
@@ -47,15 +47,15 @@ app.post("/api/message", (req,res) => {
 })
 app.get("/api/messages", (req,res) =>{
     mongoMessages.find((err,data) =>{
-        // if (err) {
-        //     res.status(500).send(err)
-        // } else {
-        //     data.sort((b,a) =>{
-        //         return a.timestamp - b.timestamp;
-        //     })
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            data.sort((b,a) =>{
+                return a.timestamp - b.timestamp;
+            })
             res.status(201).send(data)
 
-        // }
+        }
     })
 })
 
